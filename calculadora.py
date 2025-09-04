7b0a21c5585272294d8e5d894cee5228d72ca64f
Jabes Protazio <jabesprotazio@hotmail.com>
2025-09-04 08:56:44 -0300

    🟢 GREEN: Método de subtrair  implementado  na classe calculadora.  Obs; a IA está me ajudando a fazer frase de commit  mais bem elaboradas

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,12 @@ class Calculadora:
     def somar(self,numero1,numero2):
         resultado = numero1 + numero2 
         return resultado
+    
+
+    def subtrair(self, numero1, numero2):
+        return numero1 - numero2
+    
+    
+    #def dividir(self, numero1, numero2):
+        if numero2 == 0:
+            return None, "O "#
