a2426925ba45aa753fb8e6a0e82bd1a3e2c967c1
Jabes Protazio <jabesprotazio@hotmail.com>
2025-09-04 08:45:29 -0300

    🔴 RED: Teste implementado para subtração

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,8 +1,15 @@
+import pytest
 from calculadora import Calculadora
 
+@pytest.fixture 
+def calc():
+    return Calculadora()
 
-
-def test_somar():
-    call = Calculadora()
-    resultado = call.somar(1,1)
+def test_somar(calc):
+    resultado = calc.somar(1,1)
     assert resultado == 2
+
+
+def test_subtrair(calc):
+    resultado = calc.subtrair(2,1)
+    assert resultado == 1
